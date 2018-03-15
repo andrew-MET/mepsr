31f418bd941ac995b8446bf4f5a12e17be7fb8e0
Andrew Thomas Singleton <andrewts@vglserver2.met.no>
2018-03-15 17:15:23 +0000

    Added function to plot all members, or a selection of members in one plot.

--- R/quick_plot_field.R
+++ R/quick_plot_field.R
@@ -8,7 +8,7 @@
 #' @importFrom graphics lines
 #'
 #' @param eps_field Output of \code{read_members}.
-#' @param member Member to plot. Can be numeric or one of "mean", "sd".
+#' @param member Member to plot. Can be numeric or one of "mean", "sd", or "all".
 #' @param legend Whether to plot a legend - TRUE or FALSE. Defaults to TRUE.
 #' @param hires_coast Plot a high resolution coastline - TRUE or FALSE. Defaults
 #'   to FALSE.
@@ -42,12 +42,17 @@ quick_plot_field <- function(
 
   if (is.numeric(member)) {
     member_index <- which(eps_field$member == member)
-    if (length(member_index) == 0) stop("Member ", member, " not found")
-    plot_field <- eps_field$model_data[ , , member_index]
+    if (length(member_index) == 0) {
+      stop("Member ", member, " not found")
+    } else {
+      plot_field <- eps_field$model_data[ , , member_index]
+    }
   } else if (tolower(member) %in% c("mean", "sd")) {
     plot_field <- ens_mean_and_sd(
       eps_field$model_data
     )[[paste0("ens_", tolower(member))]]
+  } else if (tolower(member) == "all") {
+    plot_field <- eps_field$model_data
   } else {
     stop("Unknown option: '", member, "' for member")
   }
@@ -62,23 +67,44 @@ quick_plot_field <- function(
   }
   countries_polygon <- raster::crop(countries_polygon, raster::extent(c(x_range, y_range)))
 
+  plot_cols  <- 1
+  plot_rows  <- 1
+  num_panels <- 1
+  if (length(dim(plot_field)) == 3) {
+    num_panels <- dim(plot_field)[3]
+    plot_cols <- num_panels %>%
+      sqrt() %>%
+      ceiling()
+    plot_rows <- plot_cols - 1
+    if (plot_cols * plot_rows < num_panels) plot_rows <- plot_cols
+  }
+  panel_layout <- t(matrix(seq(1, plot_cols * plot_rows), ncol = plot_cols, nrow = plot_rows))
+  layout(panel_layout)
+  par(mar = c(1, 1, 1, 1))
   plot_fun <- ifelse (legend, fields::image.plot, image)
-  plot_fun(
-    eps_field$x,
-    eps_field$y,
-    plot_field,
-    asp  = 1,
-    xlab = "",
-    ylab = "",
-    xaxt = "n",
-    yaxt = "n",
-    bty  = "n",
-    col  = col,
-    ...
-  )
-  sp::plot(countries_polygon, add = TRUE)
-  graphics::lines(
-    c(x_range[1], x_range[2], x_range[2], x_range[1], x_range[1]),
-    c(y_range[1], y_range[1], y_range[2], y_range[2], y_range[1])
-  )
+  for (plot_panel in seq(1, num_panels)) {
+    if (num_panels == 1) {
+      panel_plot <- plot_field
+    } else {
+      panel_plot <- plot_field[, , plot_panel]
+    }
+    plot_fun(
+      eps_field$x,
+      eps_field$y,
+      panel_plot,
+      asp  = 1,
+      xlab = "",
+      ylab = "",
+      xaxt = "n",
+      yaxt = "n",
+      bty  = "n",
+      col  = col,
+      ...
+    )
+    sp::plot(countries_polygon, add = TRUE)
+    graphics::lines(
+      c(x_range[1], x_range[2], x_range[2], x_range[1], x_range[1]),
+      c(y_range[1], y_range[1], y_range[2], y_range[2], y_range[1])
+    )
+  }
 }
